7efe04fdeb9c4dbcc1b5356dc5edf83e7a576fa6
Ivan Baena <baenadev.io@gmail.com>
2021-02-03 23:48:12 -0500

    update documentation

--- src/public/apollo/server/helpers/db.ts
+++ src/public/apollo/server/helpers/db.ts
@@ -1,4 +1,3 @@
 export const MONGO_URI = `
-export const MONGO_URI =
-  'mongodb://posts-admin:mn8ZcPI20M3OfOfv@cluster0-shard-00-00.khci8.mongodb.net:27017,cluster0-shard-00-01.khci8.mongodb.net:27017,cluster0-shard-00-02.khci8.mongodb.net:27017/posts-db?ssl=true&replicaSet=atlas-j7ey9f-shard-0&authSource=admin&retryWrites=true&w=majority';
+export const MONGO_URI = ''
 `;
